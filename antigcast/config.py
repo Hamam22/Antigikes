f03b541c237e8d521faa735c4ad0c97a20d59564
Kaleng19 <157618971+Kaleng19@users.noreply.github.com>
2024-02-19 03:56:17 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -2,11 +2,11 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6722954472:AAE0BRssvdcV6RoGA7qXaYFITlcQSjMGdiw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6484737150:AAH-sBbkfVDAAUIU_Eo2Kqt7SSc6ZwRzV14")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
-APP_ID = int(os.environ.get("APP_ID", "28633257"))
-API_HASH = os.environ.get("API_HASH", "0330ec89ddab0d2bfdf8e263a68ec510")
+APP_ID = int(os.environ.get("APP_ID", "28593412"))
+API_HASH = os.environ.get("API_HASH", "eb7ab10e385bd0f592a0a9b91d75413e")
 
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002109470361"))
 
@@ -16,8 +16,8 @@ BROADCAST_AS_COPY = True
 
 PLUG = dict(root="antigcast/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "6364920674").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "Kalengsarden1")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "1909322919").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "Kaleng1")
 
 
 LOG_FILE_NAME = "antigcast_logs.txt"
@@ -36,9 +36,9 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 CREATOR = [
-    6364920674, # Kaleng
+    1909322919, # Kaleng
     1977120689, # Naka
 ]
 
-OWNER_ID.append(6364920674) # Kaleng
+OWNER_ID.append(1909322919) # Kaleng
 OWNER_ID.append(1977120689) # Naka
