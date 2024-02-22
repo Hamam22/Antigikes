e64e97cd4d96319ab48359fd8bfb8699fda9d1ff
kaleng3 <157746502+kaleng3@users.noreply.github.com>
2024-02-23 00:41:21 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -2,22 +2,22 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6484737150:AAH-sBbkfVDAAUIU_Eo2Kqt7SSc6ZwRzV14")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "isibottoken")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
-APP_ID = int(os.environ.get("APP_ID", "28593412"))
-API_HASH = os.environ.get("API_HASH", "eb7ab10e385bd0f592a0a9b91d75413e")
+APP_ID = int(os.environ.get("APP_ID", "isiapiid"))
+API_HASH = os.environ.get("API_HASH", "isiapihas")
 
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002109470361"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://KalengRobot:1234@cluster0.jjdvosf.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "KalengRobot")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "isimongodb")
+DB_NAME = os.environ.get("DB_NAME", "isinamamongo")
 BROADCAST_AS_COPY = True
 
 PLUG = dict(root="antigcast/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "1909322919").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "Kaleng1")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "isiownerid").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "isiusernametanpa@")
 
 
 LOG_FILE_NAME = "antigcast_logs.txt"
