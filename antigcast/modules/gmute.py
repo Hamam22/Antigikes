8b169e37bf0a237853f43b1a78e476e8aa6b6173
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-22 17:51:41 +0700

    Update gmute.py

--- antigcast/modules/gmute.py
+++ antigcast/modules/gmute.py
@@ -3,69 +3,73 @@ import asyncio
 from antigcast import Bot
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram.errors import FloodWait, MessageDeleteForbidden, UserNotParticipant
 
-from antigcast.config import OWNER_ID
-from antigcast.helpers.tools import extract
-from antigcast.helpers.database import get_muted_users, mute_user, unmute_user, get_actived_chats
+from antigcast.config import *
+from antigcast.helpers.tools import *
+from antigcast.helpers.admins import Admin
+from antigcast.helpers.message import get_arg, extract
+from antigcast.helpers.database import add_bl_word, remove_bl_word, get_actived_chats, get_muted_users, mute_user, unmute_user
 
 @Bot.on_message(filters.command("pl") & filters.user(OWNER_ID))
 async def mute_handler(app: Bot, message: Message):
     if not message.reply_to_message and len(message.command) != 2:
-        return await message.reply_text("Berikan saya kata terlarang yang ingin di hapus")
-        
+        return await message.reply_text("Berikan saya ID pengguna yang ingin di mute")
+
     user = await extract(message)
     user_id = user.id
 
     if user_id == message.from_user.id:
-        return await message.reply_text("Kamu tidak bisa melakukan kata terlarang pada diri sendiri")
+        return await message.reply_text("Kamu tidak bisa mute diri sendiri")
     elif user_id == app.me.id:
-        return await message.reply_text("Kamu tidak bisa melakukan kata terlarang pada Bot")
+        return await message.reply_text("Kamu tidak bisa mute bot")
     elif user_id in OWNER_ID:
-        return await message.reply_text("Kamu tidak bisa melakukan Global Delete pada Developer Bot")
+        return await message.reply_text("Kamu tidak bisa mute developer bot")
 
-    xxnx = await message.reply("`Berhasil memasukan kata terlarang...`")
+    xxnx = await message.reply("`Menambahkan pengguna ke dalam daftar mute...`")
 
     muted = await get_muted_users()
     if user_id in muted:
-        await xxnx.edit("**Maaf, Pengguna ini sudah ada di daftar kata terlarang**")
+        await xxnx.edit("**Pengguna ini sudah ada di daftar mute**")
         await asyncio.sleep(10)
         await xxnx.delete()
         await message.delete()
         return
-    
+
     try:
         kon = await app.get_users(user_id)
         kon_name = kon.first_name
+        kon_id = kon.id
 
-        await mute_user(user_id)
+        await mute_user(kon_id)
 
-        await xxnx.edit(f"**Berhasil Masukan kata terlarang**\n- Name: {kon_name}\n- User ID: `{user_id}`")
+        await xxnx.edit(f"**Pengguna berhasil di mute**\n- Nama: {kon_name}\n- User ID: `{kon_id}`")
         await asyncio.sleep(10)
         await xxnx.delete()
         await message.delete()
     except Exception as e:
-        await xxnx.edit(f"**Gagal menambahkan kata terlarang:** `{e}`")
+        await xxnx.edit(f"**Gagal mute pengguna:** `{e}`")
 
 @Bot.on_message(filters.command("ungdel") & filters.user(OWNER_ID))
 async def unmute_handler(app: Bot, message: Message):
     if not message.reply_to_message and len(message.command) != 2:
-        return await message.reply_text("Berikan saya id/username atau reply ke pesan")
-        
+        return await message.reply_text("Berikan saya ID pengguna yang ingin di unmute")
+
     user = await extract(message)
     user_id = user.id
 
     if user_id == message.from_user.id:
-        return await message.reply_text("Kamu tidak bisa melakukan Global Delete pada diri sendiri")
+        return await message.reply_text("Kamu tidak bisa unmute diri sendiri")
     elif user_id == app.me.id:
-        return await message.reply_text("Kamu tidak bisa melakukan Global Delete pada Bot")
+        return await message.reply_text("Kamu tidak bisa unmute bot")
     elif user_id in OWNER_ID:
-        return await message.reply_text("Kamu tidak bisa melakukan Global Delete pada Developer Bot")
-        
-    xxnx = await message.reply("`Membuka Global Mute..`")
+        return await message.reply_text("Kamu tidak bisa unmute developer bot")
+
+    xxnx = await message.reply("`Menghapus pengguna dari daftar mute...`")
 
     muted = await get_muted_users()
     if user_id not in muted:
-        await xxnx.edit("**Maaf, Pengguna ini tidak ada di daftar Global Mute**")
+        await xxnx.edit("**Pengguna ini tidak ada di daftar mute**")
         await asyncio.sleep(10)
         await xxnx.delete()
         await message.delete()
@@ -73,39 +77,54 @@ async def unmute_handler(app: Bot, message: Message):
 
     try:
         kon = await app.get_users(user_id)
-        kon_name = kon.mention
+        kon_name = kon.first_name
+        kon_id = kon.id
 
-        await unmute_user(user_id)
+        await unmute_user(kon_id)
 
-        await xxnx.edit(f"**Global Unmute Berhasil**\n- Name: {kon_name}\n- User ID: `{user_id}`")
+        await xxnx.edit(f"**Pengguna berhasil di unmute**\n- Nama: {kon_name}\n- User ID: `{kon_id}`")
         await asyncio.sleep(10)
         await xxnx.delete()
         await message.delete()
     except Exception as e:
-        await xxnx.edit(f"**Global Unmute Gagal:** `{e}`")
+        await xxnx.edit(f"**Gagal unmute pengguna:** `{e}`")
 
 @Bot.on_message(filters.command("gmuted") & filters.user(OWNER_ID))
 async def muted(app: Bot, message: Message):
-    kons = []
-    konlos = await get_actived_chats()
-    for user in konlos:
-        kons.append(user)
-        
+    kons = await get_muted_users()
+
     if not kons:
-        return await message.reply("**Belum Ada Daftar Gmute.**")
-    
+        return await message.reply("**Belum ada pengguna yang di mute.**")
+
     resp = await message.reply("**Memuat database...**")
 
-    msg = "**Daftar Global Mute**\n\n"
+    msg = "**Daftar pengguna yang di mute**\n\n"
     num = 0
 
     for x in kons:
         num += 1
+
         try:
             get = await app.get_users(int(x))
             gname = get.mention
-            msg += f"**{num}. {gname}**\n└ User ID: `{x}`\n\n"
-        except:
+            gid = get.id
+            msg += f"**{num}. {gname}**\n└ User ID: `{gid}`\n\n"
+        except Exception:
             msg += f"**{num}. {x}**\n\n"
 
     await resp.edit(msg, disable_web_page_preview=True)
+
+@Bot.on_message(filters.all & ~filters.private)
+async def delete_muted_messages(app: Bot, message: Message):
+    user_id = message.from_user.id
+
+    # ambil daftar pengguna kntl
+    muted_users = await get_muted_users()
+    if user_id in muted_users:
+        try:
+            await message.delete()
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await message.delete()
+        except MessageDeleteForbidden:
+            pass
