bf1ea44f0529644a055650c7e740686304dab16b
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-27 10:35:43 +0700

    Update admins.py

--- antigcast/helpers/admins.py
+++ antigcast/helpers/admins.py
@@ -1,36 +1,33 @@
 import asyncio
-from pyrogram import filters, Client
+from pyrogram import filters, enums
 from pyrogram.errors.exceptions.flood_420 import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-
 STATUS = enums.ChatMemberStatus
 
-async def is_member(filter, client, update):
+async def isMember(filter, client, update):
     try:
         member = await client.get_chat_member(chat_id=update.chat.id, user_id=update.from_user.id)
     except FloodWait as wait_err:
-        await asyncio.sleep(wait_err.seconds)
-        return False
+        await asyncio.sleep(wait_err.value)
     except UserNotParticipant:
         return False
-    except Exception:
+    except:
         return False
 
     return member.status not in [STATUS.OWNER, STATUS.ADMINISTRATOR]
 
-async def is_admin(filter, client, update):
+async def isAdmin(filter, client, update):
     try:
         member = await client.get_chat_member(chat_id=update.chat.id, user_id=update.from_user.id)
     except FloodWait as wait_err:
-        await asyncio.sleep(wait_err.seconds)
-        return False
+        await asyncio.sleep(wait_err.value)
     except UserNotParticipant:
         return False
-    except Exception:
+    except:
         return False
 
     return member.status in [STATUS.OWNER, STATUS.ADMINISTRATOR]
 
-MemberFilter = filters.create(is_member)
-AdminFilter = filters.create(is_admin)
+Member = filters.create(isMember)
+Admin = filters.create(isAdmin)
