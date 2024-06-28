e8828fdf5d57d3fc63d4f25a1b8156e7651d3e90
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-29 02:09:01 +0700

    Update imposter.py

--- antigcast/modules/imposter.py
+++ antigcast/modules/imposter.py
@@ -6,7 +6,7 @@ from antigcast import Bot, app
 
 
 
-@Bot.on_message(~filters.private & ~filters.bot & ~filters.via_bot)
+@Bot.on_message(filters.text & ~filters.private)
 async def chk_usr(app: Bot, message: Message):
     if message.sender_chat or not await check_pretender(message.chat.id):
         return
@@ -88,7 +88,7 @@ async def chk_usr(app: Bot, message: Message):
     if msg != "":
         await message.reply_photo("https://telegra.ph/file/58afe55fee5ae99d6901b.jpg", caption=msg)
 
-@app.on_message(filters.command("imposter") & ~filters.bot & ~filters.via_bot & filters.group & Admin, group=32)
+@app.on_message(filters.command("imposter") & filters.group & Admin, group=32)
 async def set_mataa(app: Bot, message: Message):
     if len(message.command) == 1:
         return await message.reply("**Penggunaan Deteksi Penyamar: penyamar enable|disable**")
