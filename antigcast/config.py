1ef90fe80e08fb3d95739e6f88244cdf2fcbba16
Mericantik <146951316+Mericantik@users.noreply.github.com>
2024-06-30 13:04:56 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -2,16 +2,16 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6633472961:AAEkiFC3Tss7tY-FWZpYN5G8BR9MiVuYNTM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6735287681:AAGxtVElNe7ro6_RarA4qxKW61w6KmiUWZc")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 APP_ID = int(os.environ.get("APP_ID", "25639252"))
 API_HASH = os.environ.get("API_HASH", "42db0fd56c51ff2b94cf064838eba7c1")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002021708108"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002070417836"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://cilikankes:kuliankes@cluster0.xtrebza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "isinamamongo")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://gihubbot80:kucings03@cluster0.cyytdoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "gihubbot80")
 BROADCAST_AS_COPY = True
 
 PLUG = dict(root="antigcast/plugins")
@@ -36,7 +36,7 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 CREATOR = [
-    843830036, # Kucing
+    6367490039, # Kucing
 ]
 
-OWNER_ID.append(843830036) # Kucing
+OWNER_ID.append(6367490039) # Kucing
