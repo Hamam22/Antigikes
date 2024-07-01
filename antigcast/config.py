9cf5056605874ee509d38248376ceb1d4781cfc1
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-07-01 18:51:13 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -1,26 +1,27 @@
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6735287681:AAGxtVElNe7ro6_RarA4qxKW61w6KmiUWZc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6633472961:AAEkiFC3Tss7tY-FWZpYN5G8BR9MiVuYNTM")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 APP_ID = int(os.environ.get("APP_ID", "25639252"))
 API_HASH = os.environ.get("API_HASH", "42db0fd56c51ff2b94cf064838eba7c1")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002070417836"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002011558545"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://nauboth:kucingankes@cluster0.8j4ullu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "nauboth")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://cilikankes:kuliankes@cluster0.xtrebza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "isinamamongo")
 BROADCAST_AS_COPY = True
 
-PLUG = dict(root="antigcast/plugins")
+PLUG = dict(root="BocilAnti/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "6367490039").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "zenithnewbie")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "843830036").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "843830036")
 
 
-LOG_FILE_NAME = "antigcast_logs.txt"
+LOG_FILE_NAME = "BocilAnti_logs.txt"
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] - %(name)s - %(message)s",
@@ -36,7 +37,7 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 CREATOR = [
-    6367490039, # Kucing
+    843830036, 
 ]
 
-OWNER_ID.append(6367490039) # Kucing
+OWNER_ID.append(843830036)
