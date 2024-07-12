49876d31bb67168d6cba0fbddef80f57925d63ad
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-07-12 10:36:37 +0700

    Update deleter.py

--- antigcast/modules/deleter.py
+++ antigcast/modules/deleter.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait, MessageDeleteForbidden, UserNotParticipan
 
 from antigcast.helpers.tools import get_arg
 from antigcast.helpers.admins import Admin
-from antigcast.helpers.database import get_bl_words, add_bl_word, remove_bl_word, get_bl_groups, get_actived_chats, add_group
+from antigcast.helpers.database import get_bl_words, add_bl_word, remove_bl_word, get_bl_groups, add_group
 
 @Bot.on_message(filters.command("bl") & ~filters.private & Admin)
 async def tambah_ke_blacklist(app: Bot, message: Message):
