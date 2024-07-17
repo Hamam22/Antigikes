14b5dff97124b014e8133877f80677a83d05efb0
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-07-17 17:37:24 +0700

    Update misc.py

--- antigcast/modules/misc.py
+++ antigcast/modules/misc.py
@@ -11,7 +11,32 @@ from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
+from datetime import datetime
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
 
+# Fungsi untuk mendapatkan informasi file dari pesan
+def get_file_id(message):
+    if message.photo:
+        return {
+            'message_type': 'Foto',
+            'file_id': message.photo.file_id
+        }
+    elif message.document:
+        return {
+            'message_type': 'Dokumen',
+            'file_id': message.document.file_id
+        }
+    # Tangani jenis file lain atau kembalikan None jika tidak ada file
+    return None
+
+# Fungsi untuk mengekstrak ID pengguna dan username dari pesan
+def extract_user(message):
+    from_user_id = message.from_user.id
+    username = message.from_user.username  # Opsional, tergantung kebutuhan Anda
+    return from_user_id, username
+
+# Perintah untuk menampilkan informasi ID pengguna atau obrolan
 @Bot.on_message(filters.command('id'))
 async def show_id(client, message):
     chat_type = message.chat.type
@@ -21,37 +46,36 @@ async def show_id(client, message):
         last = message.from_user.last_name or ""
         username = message.from_user.username
         dc_id = message.from_user.dc_id or ""
-        await message.reply_text(f"<b>➲ ꜰɪʀꜱᴛ ɴᴀᴍᴇ:</b> {first}\n<b>➲ ʟᴀꜱᴛ ɴᴀᴍᴇ:</b> {last}\n<b>➲ ᴜꜱᴇʀɴᴀᴍᴇ:</b> {username}\n<b>➲ ᴛᴇʟᴇɢʀᴀᴍ ɪᴅ:</b> <code>{user_id}</code>\n<b>➲ ᴅᴄ ɪᴅ:</b> <code>{dc_id}</code>", quote=True)
+        await message.reply_text(f"➲ Nama Depan: {first}\n➲ Nama Belakang: {last}\n➲ Username: {username}\n➲ ID Telegram: {user_id}\n➲ ID DC: {dc_id}", quote=True)
 
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         _id = ""
-        _id += f"<b>➲ ᴄʜᴀᴛ ɪᴅ</b>: <code>{message.chat.id}</code>\n"
+        _id += f"➲ ID Obrolan: {message.chat.id}\n"
         
         if message.reply_to_message:
+            from_user_id, _ = extract_user(message)
+            replied_user_id = message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'
             _id += (
-                "<b>➲ ᴜꜱᴇʀ ɪᴅ</b>: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
-                "<b>➲ ʀᴇᴩʟɪᴇᴅ ᴜꜱᴇʀ ɪᴅ</b>: "
-                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
+                f"➲ ID Pengguna: {from_user_id}\n"
+                f"➲ ID Pengguna yang Dibalas: {replied_user_id}\n"
             )
             file_info = get_file_id(message.reply_to_message)
         else:
+            from_user_id, _ = extract_user(message)
             _id += (
-                "<b>➲ ᴜꜱᴇʀ ɪᴅ</b>: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
+                f"➲ ID Pengguna: {from_user_id}\n"
             )
             file_info = get_file_id(message)
         if file_info:
             _id += (
-                f"<b>{file_info.message_type}</b>: "
-                f"<code>{file_info.file_id}</code>\n"
+                f"{file_info['message_type']}: {file_info['file_id']}\n"
             )
         await message.reply_text(_id, quote=True)
-            
 
+# Perintah untuk menampilkan informasi pengguna
 @Bot.on_message(filters.command(["info"]))
 async def user_info(client, message):
-    status_message = await message.reply_text("`ᴩʟᴇᴀꜱᴇ ᴡᴀɪᴛ....`")
+    status_message = await message.reply_text("`Mohon tunggu....`")
     from_user = None
     from_user_id, _ = extract_user(message)
     try:
@@ -59,46 +83,45 @@ async def user_info(client, message):
     except Exception as error:
         return await status_message.edit(str(error))
     if from_user is None:
-        return await status_message.edit("ɴᴏ ᴠᴀʟɪᴅ ᴜsᴇʀ_ɪᴅ / ᴍᴇssᴀɢᴇ sᴘᴇᴄɪғɪᴇᴅ")
+        return await status_message.edit("ID pengguna tidak valid atau pesan tidak spesifik")
     message_out_str = ""
-    message_out_str += f"<b>➲ꜰɪʀꜱᴛ ɴᴀᴍᴇ:</b> {from_user.first_name}\n"
-    last_name = from_user.last_name or "<b>ɴᴏɴᴇ</b>"
-    message_out_str += f"<b>➲ʟᴀꜱᴛ ɴᴀᴍᴇ:</b> {last_name}\n"
-    message_out_str += f"<b>➲ᴛɢ-ɪᴅ:</b> <code>{from_user.id}</code>\n"
-    username = from_user.username or "<b>ɴᴏɴᴇ</b>"
-    dc_id = from_user.dc_id or "[ᴜꜱᴇʀ ᴅᴏꜱᴇ'ᴛ ʜᴀᴠᴇ ᴀ ᴠᴀʟɪᴅ ᴅᴩ]"
-    message_out_str += f"<b>➲ᴅᴄ-ɪᴅ:</b> <code>{dc_id}</code>\n"
-    message_out_str += f"<b>➲ᴜꜱᴇʀɴᴀᴍᴇ:</b> @{username}\n"
-    message_out_str += f"<b>➲ᴜꜱᴇʀ ʟɪɴᴋ:</b> <a href='tg://user?id={from_user.id}'><b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</b></a>\n"
+    message_out_str += f"➲ Nama Depan: {from_user.first_name}\n"
+    last_name = from_user.last_name or "Tidak ada"
+    message_out_str += f"➲ Nama Belakang: {last_name}\n"
+    message_out_str += f"➲ ID Telegram: {from_user.id}\n"
+    username = from_user.username or "Tidak ada"
+    dc_id = from_user.dc_id or "[Pengguna tidak memiliki DP yang valid]"
+    message_out_str += f"➲ ID DC: {dc_id}\n"
+    message_out_str += f"➲ Username: @{username}\n"
+    message_out_str += f"➲ Tautan Pengguna: [Klik di sini](tg://user?id={from_user.id})\n"
     if message.chat.type in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
         try:
             chat_member_p = await message.chat.get_member(from_user.id)
             joined_date = (chat_member_p.joined_date or datetime.now()).strftime("%Y.%m.%d %H:%M:%S")
-            message_out_str += f"<b>➲ᴊᴏɪɴᴇᴅ ᴛʜɪꜱ ᴄʜᴀᴛ ᴏɴ:</b> <code>{joined_date}</code>\n"
-        except UserNotParticipant: pass
+            message_out_str += f"➲ Bergabung dalam obrolan ini pada: {joined_date}\n"
+        except UserNotParticipant:
+            pass
     chat_photo = from_user.photo
     if chat_photo:
         local_user_photo = await client.download_media(message=chat_photo.big_file_id)
-        buttons = [[InlineKeyboardButton('ᴄʟᴏꜱᴇ ✘', callback_data='close_data')]]
+        buttons = [[InlineKeyboardButton('Tutup ✘', callback_data='close_data')]]
         await message.reply_photo(
             photo=local_user_photo,
             quote=True,
             reply_markup=InlineKeyboardMarkup(buttons),
             caption=message_out_str,
-            parse_mode=enums.ParseMode.HTML,
             disable_notification=True
         )
         os.remove(local_user_photo)
     else:
-        buttons = [[InlineKeyboardButton('ᴄʟᴏꜱᴇ ✘', callback_data='close_data')]]
+        buttons = [[InlineKeyboardButton('Tutup ✘', callback_data='close_data')]]
         await message.reply_text(
             text=message_out_str,
             reply_markup=InlineKeyboardMarkup(buttons),
             quote=True,
-            parse_mode=enums.ParseMode.HTML,
             disable_notification=True
         )
-    await status_message.delete()
+    await status_message.delete()    
 
 @Bot.on_message(filters.command(['song', 'mp3']) & filters.group, group=46)
 async def song(client, message):
