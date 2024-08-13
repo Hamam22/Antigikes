8c1b41b93c6110b4de15ba3f3d2aeb634bd73773
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-08-13 14:33:25 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -3,7 +3,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6048377445:AAEw5_rEmTLGw4K9xy5NLsEZKTOdc0oYvQE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6633472961:AAEkiFC3Tss7tY-FWZpYN5G8BR9MiVuYNTM")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 APP_ID = int(os.environ.get("APP_ID", "25639252"))
@@ -11,14 +11,14 @@ API_HASH = os.environ.get("API_HASH", "42db0fd56c51ff2b94cf064838eba7c1")
 
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002011558545"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://uancoks67:dancoks89@cluster0.pgonf3i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_DB_URI", "mongodb+srv://cilikankes:kuliankes@cluster0.xtrebza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "isinamamongo")
 BROADCAST_AS_COPY = True
 
 PLUG = dict(root="BocilAnti/plugins")
 
 OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "843830036").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "babah")
+OWNER_NAME = os.environ.get("OWNER_NAME", "843830036")
 
 
 LOG_FILE_NAME = "BocilAnti_logs.txt"
@@ -31,7 +31,7 @@ logging.basicConfig(
         logging.StreamHandler(),
     ],
 )
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
