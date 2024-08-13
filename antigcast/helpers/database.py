dad7097a3bfbf59a26cfe06bd6ab129c5e2588e0
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-08-13 10:56:15 +0700

    Update database.py

--- antigcast/helpers/database.py
+++ antigcast/helpers/database.py
@@ -139,35 +139,28 @@ async def rem_actived_chat(trigger) -> bool:
 
 
 #BLWORD
-async def get_bl_words(chat_id) -> list:
-    group = await blackword.find_one({"chat_id": chat_id})
-    if not group or "filters" not in group:
+async def get_bl_words() -> list:
+    filters = await blackword.find_one({"filter": "filter"})
+    if not filters:
         return []
-    return group["filters"]
+    return filters["filters"]
 
-async def add_bl_word(trigger, user_info) -> bool:
+
+async def add_bl_word(trigger) -> bool:
     x = trigger.lower()
-    filters = await get_bl_words(user_info["chat_id"])
+    filters = await get_bl_words()
     filters.append(x)
-    
-    await blackword.update_one(
-        {"chat_id": user_info["chat_id"]},
-        {"$set": {"filters": filters, "group_name": user_info["group_name"]}},
-        upsert=True
-    )
+    await blackword.update_one({"filter": "filter"}, {"$set": {"filters": filters}}, upsert=True)
     return True
 
-async def remove_bl_word(trigger, chat_id) -> bool:
+
+async def remove_bl_word(trigger) -> bool:
     x = trigger.lower()
-    filters = await get_bl_words(chat_id)
+    filters = await get_bl_words()
     filters.remove(x)
-    await blackword.update_one({"chat_id": chat_id}, {"$set": {"filters": filters}}, upsert=True)
+    await blackword.update_one({"filter": "filter"}, {"$set": {"filters": filters}}, upsert=True)
     return True
-
-async def get_bl_groups():
-    groups = await blackword.find().to_list(None)
-    return [{"group_name": group["group_name"], "chat_id": group["chat_id"]} for group in groups if "group_name" in group]
-
+    
 # OWNER
 async def get_owners() -> list:
     owners = await owner.find_one({"owner": "owner"})
